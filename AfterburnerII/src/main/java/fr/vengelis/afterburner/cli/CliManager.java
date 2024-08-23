91003db59296049c3f2f0a99ff3039b6e9306beb
Vengelis_ <gaby17250@gmail.com>
2024-08-23 12:30:53 +0200

    fix : improve input command

--- AfterburnerII/src/main/java/fr/vengelis/afterburner/cli/CliManager.java
+++ AfterburnerII/src/main/java/fr/vengelis/afterburner/cli/CliManager.java
@@ -41,7 +41,9 @@ public class CliManager {
                         .setAction(arg -> {
                             String cmd = String.join(" ", arg);
                             ConsoleLogger.printLine(Level.INFO, "Command sended > " + cmd);
-                            AfterburnerApp.get().sendCommandToProcess(cmd);
+                            if(!AfterburnerApp.get().sendCommandToProcess(cmd)) {
+                                ConsoleLogger.printLine(Level.SEVERE, "Command not sended !");
+                            }
                         })
                         .build())
                 .addSubCommand(new AtbCommand.AtbCommandBuilder(AtbCommand.State.FINAL)
