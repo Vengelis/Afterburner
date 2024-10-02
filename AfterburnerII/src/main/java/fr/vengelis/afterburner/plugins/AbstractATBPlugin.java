0df81980e2d785392d9f99ab36ef847987e6832d
Vengelis_ <gaby17250@gmail.com>
2024-10-02 11:58:15 +0200

    update : improve abstract plugin

--- AfterburnerII/src/main/java/fr/vengelis/afterburner/plugins/AbstractATBPlugin.java
+++ AfterburnerII/src/main/java/fr/vengelis/afterburner/plugins/AbstractATBPlugin.java
@@ -1,6 +1,14 @@
 package fr.vengelis.afterburner.plugins;
 
+import fr.vengelis.afterburner.Afterburner;
+
+import java.io.File;
+
 public abstract class AbstractATBPlugin {
+
+    protected String getWorkingDirectory() {
+        return Afterburner.WORKING_AREA + File.separator + "plugins" + File.separator;
+    }
 
     public abstract void onLoad();
 
