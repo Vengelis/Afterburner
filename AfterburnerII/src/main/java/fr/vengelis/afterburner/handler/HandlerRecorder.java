e62f561b2664feaac926b33d97a61c5a82c89be8
Vengelis_ <gaby17250@gmail.com>
2024-10-22 14:47:25 +0200

    update : add broadcaster in preinit event

--- AfterburnerII/src/main/java/fr/vengelis/afterburner/handler/HandlerRecorder.java
+++ AfterburnerII/src/main/java/fr/vengelis/afterburner/handler/HandlerRecorder.java
@@ -1,6 +1,7 @@
 package fr.vengelis.afterburner.handler;
 
 import fr.vengelis.afterburner.Afterburner;
+import fr.vengelis.afterburner.AfterburnerBroadcasterApp;
 import fr.vengelis.afterburner.AfterburnerClientApp;
 import fr.vengelis.afterburner.AfterburnerSlaveApp;
 import fr.vengelis.afterburner.events.impl.common.PreInitEvent;
@@ -36,6 +37,8 @@ public class HandlerRecorder {
             AfterburnerSlaveApp.get().getEventManager().call(new PreInitEvent());
         else if(Afterburner.getLaunchType().equals(Afterburner.LaunchType.PANEL))
             AfterburnerClientApp.get().getEventManager().call(new PreInitEvent());
+        else if(Afterburner.getLaunchType().equals(Afterburner.LaunchType.BROADCASTER))
+            AfterburnerBroadcasterApp.get().getEventManager().call(new PreInitEvent());
     }
 
     public static HandlerRecorder get() {
